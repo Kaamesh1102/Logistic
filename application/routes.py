bf646de5fa590d2474efa69f2bae909fcb94a338
Kaamesh1102 <21f2000761@ds.study.iitm.ac.in.>
2025-03-24 23:21:19 +0530

    2.3

--- application/routes.py
+++ application/routes.py
@@ -1,8 +1,16 @@
-from flask import current_app as app
-from flask_security import auth_required
+from flask import current_app as app,jsonify
+from flask_security import auth_required,roles_required
 
 @app.route('/admin')
 @auth_required('token')
+@roles_required('admin')
 def admin_home():
-    return "<h1>this is admin <\h1>"\
+    return {
+        "message" : "admin logged in"
+    }
     
+@app.route('/user')
+def user_home():
+    return jsonify({
+        
+    })
